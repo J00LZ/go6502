35f9247eda0a336d50d23a26f1f92552817ce533
Julius de Jeu <julius@voidcorp.nl>
2021-03-23 14:02:49 +0100

    explain the assembly

--- cmd/go6502/main.go
+++ cmd/go6502/main.go
@@ -14,12 +14,12 @@ func main() {
 		Name:     "RAM",
 	}
 	romm := make([]byte, 0x8000)
-	romm[0] = 0xA9
-	romm[1] = 0x01
-	romm[2] = 0xAA
-	romm[3] = 0x4C
-	romm[4] = 0x03
-	romm[5] = 0x80
+	romm[0] = 0xA9 // lda
+	romm[1] = 0x01 // #$01
+	romm[2] = 0xAA // tax
+	romm[3] = 0x4C // JMP
+	romm[4] = 0x03 // #$03
+	romm[5] = 0x80 // #$80
 	romm[cpu.RstVectorH-0x8000] = 0x80
 	romm[cpu.RstVectorL-0x8000] = 0x00
 	rom := &bus.ByteBus{StartVal: 0x8000, Arr: romm, Name: "ROM"}
